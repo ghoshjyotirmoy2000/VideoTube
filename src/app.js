f1f5547037f4afa10dccf329f6069568fc6f8300
jyotirmoyGhosh <ghoshjotirmoy368@gmail.com>
2025-11-23 16:25:21 +0530

    added : healthe check flow

--- src/app.js
+++ src/app.js
@@ -1,23 +1,37 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
+
 app.use(
-    cors({
-        origin : process.env.CORS_ORIGIN,
-        credentials : true
-    })
-)
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(express.json({
-    limit : "20kb"
-}))
+app.use(
+  express.json({
+    limit: "20kb",
+  })
+);
 
-app.use(express.urlencoded({
-    extended : true,
-    limit : "16kb"
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-export default app
+// import routes
+import healthCheckRoutes from "./routes/healthCheck.routes.js";
+
+app.get("/", (req, res) => {
+    res.send("Welcome to VideoTube")
+})
+
+app.use("/api/v1/healtheCheck" , healthCheckRoutes)
+
+export default app;
